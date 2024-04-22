fdfa8b04376193ed6a287a617a77c4cbe87c022e
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-22 16:37:50 +0100

    Initial commit

---
+++ src/main/java/net/jazbelt/springboot3jpahibernate/Springboot3JpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package net.jazbelt.springboot3jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3JpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3JpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jazbelt/springboot3jpahibernate/Springboot3JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jazbelt.springboot3jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
